862e2a220ca725824ed4e1ab9a5dba7909bb10b2
ponzaa567 <ponpon13173@gmail.com>
2024-11-25 22:02:14 +0700

    Login with Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client'
 import SlideHome from "@/components/home_content/SlideHome";
 import Welcome from "@/components/welcome_content/Welcome";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
+  const {data:session} = useSession()
   return (
     <>
       <SlideHome />
